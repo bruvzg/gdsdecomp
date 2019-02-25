40eae34a8fe5ac959921379ddd1a36f252ff3997
bruvzg <7645683+bruvzg@users.noreply.github.com>
2019-02-25 16:15:32 +0200

    [Standalone] Fix log window selection

--- standalone/main.gd
+++ standalone/main.gd
@@ -2,3 +2,6 @@ extends Control
 
 func _on_re_editor_standalone_write_log_message(message):
 	$log_window.text += message
+
+func _on_version_lbl_pressed():
+	OS.shell_open("https://github.com/bruvzg/gdsdecomp")
