1fb886b215213b4e660b31701ef2e96ccdd91e38
nikitalita <69168929+nikitalita@users.noreply.github.com>
2022-09-25 12:06:49 -0700

    add initial input event parsing support (not working)

---
+++ utility/input_event_parser_v2.cpp
@@ -0,0 +1,6 @@
+#include "input_event_parser_v2.h"
+#include <core/input/input_event.h>
+
+Error InputEventParserV2::parse_input_event_construct_v2(VariantParser::Stream *p_stream, Variant &r_v, int &line, String &r_err_str) {
+	ERR_FAIL_V_MSG(ERR_UNAVAILABLE, "Godot 2.x Input Event variant parsing not implemented yet");
+}
