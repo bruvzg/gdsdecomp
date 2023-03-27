ab383e5f9203d22ee4b36a1e0cc70c4c0b21d481
nikitalita <69168929+nikitalita@users.noreply.github.com>
2023-03-27 01:10:52 -0700

    Improve getting pck offset from self-contained exe

--- utility/gdre_packed_source.cpp
+++ utility/gdre_packed_source.cpp
@@ -2,6 +2,193 @@
 #include "core/io/file_access_encrypted.h"
 #include "gdre_settings.h"
 
+uint64_t get_offset_unix(const String &p_path) {
+	Ref<FileAccess> f = FileAccess::open(p_path, FileAccess::READ);
+
+	if (f.is_null()) {
+		return 0;
+	}
+
+	// Read and check ELF magic number.
+	{
+		uint32_t magic = f->get_32();
+		if (magic != 0x464c457f) { // 0x7F + "ELF"
+			return 0;
+		}
+	}
+
+	// Read program architecture bits from class field.
+	int bits = f->get_8() * 32;
+
+	// Get info about the section header table.
+	int64_t section_table_pos;
+	int64_t section_header_size;
+	if (bits == 32) {
+		section_header_size = 40;
+		f->seek(0x20);
+		section_table_pos = f->get_32();
+		f->seek(0x30);
+	} else { // 64
+		section_header_size = 64;
+		f->seek(0x28);
+		section_table_pos = f->get_64();
+		f->seek(0x3c);
+	}
+	int num_sections = f->get_16();
+	int string_section_idx = f->get_16();
+
+	// Load the strings table.
+	uint8_t *strings;
+	{
+		// Jump to the strings section header.
+		f->seek(section_table_pos + string_section_idx * section_header_size);
+
+		// Read strings data size and offset.
+		int64_t string_data_pos;
+		int64_t string_data_size;
+		if (bits == 32) {
+			f->seek(f->get_position() + 0x10);
+			string_data_pos = f->get_32();
+			string_data_size = f->get_32();
+		} else { // 64
+			f->seek(f->get_position() + 0x18);
+			string_data_pos = f->get_64();
+			string_data_size = f->get_64();
+		}
+
+		// Read strings data.
+		f->seek(string_data_pos);
+		strings = (uint8_t *)memalloc(string_data_size);
+		if (!strings) {
+			return 0;
+		}
+		f->get_buffer(strings, string_data_size);
+	}
+
+	// Search for the "pck" section.
+	int64_t off = 0;
+	for (int i = 0; i < num_sections; ++i) {
+		int64_t section_header_pos = section_table_pos + i * section_header_size;
+		f->seek(section_header_pos);
+
+		uint32_t name_offset = f->get_32();
+		if (strcmp((char *)strings + name_offset, "pck") == 0) {
+			if (bits == 32) {
+				f->seek(section_header_pos + 0x10);
+				off = f->get_32();
+			} else { // 64
+				f->seek(section_header_pos + 0x18);
+				off = f->get_64();
+			}
+			break;
+		}
+	}
+	memfree(strings);
+	return off;
+}
+
+uint64_t get_offset_windows(const String &p_path) {
+	Ref<FileAccess> f = FileAccess::open(p_path, FileAccess::READ);
+	if (f.is_null()) {
+		return 0;
+	}
+	// Process header.
+	{
+		f->seek(0x3c);
+		uint32_t pe_pos = f->get_32();
+
+		f->seek(pe_pos);
+		uint32_t magic = f->get_32();
+		if (magic != 0x00004550) {
+			return 0;
+		}
+	}
+
+	int num_sections;
+	{
+		int64_t header_pos = f->get_position();
+
+		f->seek(header_pos + 2);
+		num_sections = f->get_16();
+		f->seek(header_pos + 16);
+		uint16_t opt_header_size = f->get_16();
+
+		// Skip rest of header + optional header to go to the section headers.
+		f->seek(f->get_position() + 2 + opt_header_size);
+	}
+	int64_t section_table_pos = f->get_position();
+
+	// Search for the "pck" section.
+	int64_t off = 0;
+	for (int i = 0; i < num_sections; ++i) {
+		int64_t section_header_pos = section_table_pos + i * 40;
+		f->seek(section_header_pos);
+
+		uint8_t section_name[9];
+		f->get_buffer(section_name, 8);
+		section_name[8] = '\0';
+
+		if (strcmp((char *)section_name, "pck") == 0) {
+			f->seek(section_header_pos + 20);
+			off = f->get_32();
+			break;
+		}
+	}
+	return off;
+}
+
+bool seek_offset_from_exe(Ref<FileAccess> f, const String &p_path, uint64_t p_offset) {
+	uint64_t off = 0;
+	bool pck_header_found = false;
+	uint32_t magic = 0;
+	// Loading with offset feature not supported for self contained exe files.
+	if (p_offset != 0) {
+		ERR_FAIL_V_MSG(false, "Loading self-contained executable with offset not supported.");
+	}
+
+	int64_t pck_off = p_path.get_extension() == "exe" ? get_offset_windows(p_path) : get_offset_unix(p_path);
+	if (pck_off != 0) {
+		// Search for the header, in case PCK start and section have different alignment.
+		for (int i = 0; i < 8; i++) {
+			f->seek(pck_off);
+			magic = f->get_32();
+			if (magic == PACK_HEADER_MAGIC) {
+#ifdef DEBUG_ENABLED
+				print_verbose("PCK header found in executable pck section, loading from offset 0x" + String::num_int64(pck_off - 4, 16));
+#endif
+				return true;
+			}
+			pck_off++;
+		}
+	}
+
+	// Search for the header at the end of file - self contained executable.
+	if (!pck_header_found) {
+		// Loading with offset feature not supported for self contained exe files.
+		if (p_offset != 0) {
+			ERR_FAIL_V_MSG(false, "Loading self-contained executable with offset not supported.");
+		}
+
+		f->seek_end();
+		f->seek(f->get_position() - 4);
+		magic = f->get_32();
+
+		if (magic == PACK_HEADER_MAGIC) {
+			f->seek(f->get_position() - 12);
+			uint64_t ds = f->get_64();
+			f->seek(f->get_position() - ds - 8);
+			magic = f->get_32();
+			if (magic == PACK_HEADER_MAGIC) {
+#ifdef DEBUG_ENABLED
+				print_verbose("PCK header found at the end of executable, loading from offset 0x" + String::num_int64(f->get_position() - 4, 16));
+#endif
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
 bool GDREPackedSource::try_open_pack(const String &p_path, bool p_replace_files, uint64_t p_offset) {
 	if (p_path.get_extension().to_lower() == "apk" || p_path.get_extension().to_lower() == "zip") {
 		return false;
@@ -18,25 +205,7 @@ bool GDREPackedSource::try_open_pack(const String &p_path, bool p_replace_files,
 	uint32_t magic = f->get_32();
 
 	if (magic != PACK_HEADER_MAGIC) {
-		// loading with offset feature not supported for self contained exe files
-		if (p_offset != 0) {
-			ERR_FAIL_V_MSG(false, "Loading self-contained executable with offset not supported.");
-		}
-
-		//maybe at the end.... self contained exe
-		f->seek_end();
-		f->seek(f->get_position() - 4);
-		magic = f->get_32();
-		if (magic != PACK_HEADER_MAGIC) {
-			return false;
-		}
-		f->seek(f->get_position() - 12);
-
-		uint64_t ds = f->get_64();
-		f->seek(f->get_position() - ds - 8);
-
-		magic = f->get_32();
-		if (magic != PACK_HEADER_MAGIC) {
+		if (!seek_offset_from_exe(f, p_path, p_offset)) {
 			return false;
 		}
 		is_exe = true;
@@ -107,7 +276,7 @@ bool GDREPackedSource::try_open_pack(const String &p_path, bool p_replace_files,
 		String path;
 		path.parse_utf8(cs.ptr());
 
-		ERR_FAIL_COND_V_MSG(path.get_file().find("gdre_") != -1, false, "Tried to load a gdre file?!?!");
+		ERR_FAIL_COND_V_MSG(path.get_file().find("gdre_") != -1, false, "Don't try to extract the GDRE pack files, just download the source from github.");
 
 		uint64_t ofs = file_base + f->get_64();
 		uint64_t size = f->get_64();
