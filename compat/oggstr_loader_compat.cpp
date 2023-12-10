8af891739b7a49128967adce3ddb674732b30371
nikitalita <69168929+nikitalita@users.noreply.github.com>
2023-12-10 03:02:07 -0800

    Add v4 ogg vorbis export support

--- compat/oggstr_loader_compat.cpp
+++ compat/oggstr_loader_compat.cpp
@@ -1,19 +1,86 @@
 #include "oggstr_loader_compat.h"
+#include "core/io/file_access.h"
+#include "ogg/ogg.h"
 #include "resource_loader_compat.h"
 #include "utility/gdre_settings.h"
+#include "vorbis/codec.h"
 
-#include "core/io/file_access.h"
-
-Vector<uint8_t> packet_data_to_byte_array(Array page_data) {
-	Vector<uint8_t> data;
+Error packet_sequence_to_raw_data(const Ref<OggPacketSequence> &packet_sequence, Vector<uint8_t> &r_data) {
+	auto page_data = packet_sequence->get_packet_data();
+	Vector<uint64_t> page_sizes;
+	uint64_t total_estimated_size = 0;
+	// page data is a vector of a vector of packedbytearrays
+	// need to get all the sizes of the pages
 	for (int i = 0; i < page_data.size(); i++) {
-		Array page_variant = page_data.get(i);
-		for (int j = 0; j < page_variant.size(); j++) {
-			const PackedByteArray &packet = page_variant.get(j);
-			data.append_array(packet);
+		uint64_t page_size = 0;
+		// a page is an array of PackedByteArrays
+		Array page = page_data[i];
+		for (int j = 0; j < page.size(); j++) {
+			int pkt_size = ((PackedByteArray)page[j]).size();
+			page_size += pkt_size;
+			// Header size is 26 + ceil(pkt_size / 255)
+			total_estimated_size += pkt_size + (pkt_size / 255) + 100; //just fudge it
+		}
+		page_sizes.push_back(page_size);
+	}
+	uint64_t total_pagedata_body_size = [](const Vector<uint64_t> p_counts) {
+		uint64_t total = 0;
+		for (int i = 0; i < p_counts.size(); i++) {
+			total += p_counts[i];
+		}
+		return total;
+	}(page_sizes);
+	uint64_t total_actual_body_size = 0;
+	uint64_t total_acc_size = 0;
+	auto playback = packet_sequence->instantiate_playback();
+	ogg_packet *pkt;
+	ogg_stream_state os_en;
+	ogg_stream_init(&os_en, rand());
+	int page_cursor = 0;
+	bool reached_eos = false;
+	bool force_break = false;
+	r_data.resize_zeroed(total_estimated_size);
+	// next_ogg_packet is broken and will keep returning packets after EOS
+	while (playback->next_ogg_packet(&pkt)) {
+		int page_size = 4096;
+		page_size = page_sizes[page_cursor];
+		if (pkt->e_o_s) {
+			reached_eos = true;
+		}
+		ogg_stream_packetin(&os_en, pkt);
+		ERR_FAIL_COND_V_MSG(ogg_stream_check(&os_en), ERR_FILE_CORRUPT, "Ogg stream is corrupt.");
+		if (os_en.body_fill >= page_size) {
+			ogg_page og;
+			ERR_FAIL_COND_V_MSG(ogg_stream_flush_fill(&os_en, &og, page_size) == 0, ERR_FILE_CORRUPT, "Could not add page.");
+			int cur_pos = total_acc_size;
+			total_acc_size += og.header_len + og.body_len;
+			total_actual_body_size += og.body_len;
+			if (total_acc_size > r_data.size()) {
+				WARN_PRINT("Resizing data to " + itos(total_acc_size));
+				r_data.resize(total_acc_size);
+			}
+			memcpy(r_data.ptrw() + cur_pos, og.header, og.header_len);
+			memcpy(r_data.ptrw() + cur_pos + og.header_len, og.body, og.body_len);
+			page_cursor++;
+			if (page_cursor >= page_sizes.size() || reached_eos) {
+				break;
+			}
 		}
 	}
-	return data;
+	if (total_actual_body_size != total_pagedata_body_size) {
+		WARN_PRINT("Actual body size" + itos(total_actual_body_size) + " does not equal the pagedata body size " + itos(total_pagedata_body_size) + ".");
+	}
+
+	ogg_stream_clear(&os_en);
+	ERR_FAIL_COND_V_MSG(!reached_eos, ERR_FILE_CORRUPT, "All packets consumed before EOS.");
+	ERR_FAIL_COND_V_MSG(page_cursor < page_sizes.size(), ERR_FILE_CORRUPT, "Did not write all pages before EOS.");
+	ERR_FAIL_COND_V_MSG(r_data.size() < 4, ERR_FILE_CORRUPT, "Data is too small to be an Ogg stream.");
+	ERR_FAIL_COND_V_MSG(r_data[0] != 'O' || r_data[1] != 'g' || r_data[2] != 'g' || r_data[3] != 'S', ERR_FILE_CORRUPT, "Header is missing in ogg data.");
+
+	// resize to the actual size
+	r_data.resize(total_acc_size);
+	// check if r_data begins with "OggS"
+	return OK;
 }
 
 Vector<uint8_t> OggStreamLoaderCompat::get_ogg_stream_data(const String &p_path, Error *r_err) const {
@@ -35,13 +102,16 @@ Vector<uint8_t> OggStreamLoaderCompat::get_ogg_stream_data(const String &p_path,
 	Vector<uint8_t> data;
 	ERR_RFLBC_COND_V_MSG_CLEANUP(err != OK, Vector<uint8_t>(), "Cannot open resource '" + p_path + "'.", loader);
 	if (loader->engine_ver_major == 4) {
+		memdelete(loader);
 		Ref<AudioStreamOggVorbis> sample = ResourceLoader::load(p_path, "", ResourceFormatLoader::CACHE_MODE_IGNORE, &err);
 		if (r_err) {
 			*r_err = err;
 		}
-		ERR_RFLBC_COND_V_MSG_CLEANUP(err != OK, Vector<uint8_t>(), "Cannot open resource '" + p_path + "'.", loader);
-		memdelete(loader);
-		return packet_data_to_byte_array(sample->get_packet_sequence()->get_packet_data());
+		ERR_FAIL_COND_V_MSG(err != OK, Vector<uint8_t>(), "Cannot open resource '" + p_path + "'.");
+		auto packet_sequence = sample->get_packet_sequence();
+		Error err = packet_sequence_to_raw_data(packet_sequence, data);
+		ERR_FAIL_COND_V_MSG(err != OK, Vector<uint8_t>(), "Cannot convert packet sequence to raw data.");
+		return data;
 	}
 	err = loader->load();
 	if (r_err) {
