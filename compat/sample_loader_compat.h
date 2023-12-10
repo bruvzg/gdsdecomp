97fbca1d15e8ad7798b1af2ee4ee658db43cbed5
nikitalita <69168929+nikitalita@users.noreply.github.com>
2023-12-09 21:28:24 -0800

    refactor sample loading, add v2 support

---
+++ compat/sample_loader_compat.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "core/io/file_access.h"
+#include "core/io/image.h"
+#include "core/io/resource.h"
+#include "core/io/resource_loader.h"
+#include "core/object/ref_counted.h"
+#include "core/templates/vector.h"
+#include "modules/vorbis/audio_stream_ogg_vorbis.h"
+#include "scene/resources/audio_stream_wav.h"
+#include "scene/resources/texture.h"
+
+class SampleLoaderCompat : public RefCounted {
+	GDCLASS(SampleLoaderCompat, RefCounted);
+
+protected:
+	static void _bind_methods();
+
+public:
+	static Ref<AudioStreamWAV> load_wav(const String &p_path, Error *r_err);
+	static Ref<AudioStreamWAV> convert_adpcm_to_16bit(const Ref<AudioStreamWAV> &p_sample);
+};
