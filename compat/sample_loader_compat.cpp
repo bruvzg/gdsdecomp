fa0b54bcbbc917b775b3e51c8a7260c8ff21c07a
nikitalita <69168929+nikitalita@users.noreply.github.com>
2024-10-27 12:28:24 -0500

    fix sample loader

--- compat/sample_loader_compat.cpp
+++ compat/sample_loader_compat.cpp
@@ -2,7 +2,7 @@
 #include "scene/resources/audio_stream_wav.h"
 
 Ref<Resource> SampleConverterCompat::convert(const Ref<MissingResource> &res, ResourceInfo::LoadType p_type, int ver_major, Error *r_error) {
-	String name = ver_major < 3 ? res->get("resource/name") : res->get("resource_name");
+	String name = get_resource_name(res, ver_major);
 	AudioStreamWAV::LoopMode loop_mode = (AudioStreamWAV::LoopMode) int(res->get("loop_format"));
 	// int loop_begin, loop_end, mix_rate, data_bytes = 0;
 	int loop_begin = res->get("loop_begin");
@@ -57,5 +57,5 @@ Ref<Resource> SampleConverterCompat::convert(const Ref<MissingResource> &res, Re
 }
 
 bool SampleConverterCompat::handles_type(const String &p_type, int ver_major) const {
-	return (p_type == "AudioStreamWAV" || p_type == "AudioStreamSample") && ver_major < 4;
+	return (p_type == "AudioStreamWAV" || p_type == "AudioStreamSample" || p_type == "Sample") && ver_major < 4;
 }
