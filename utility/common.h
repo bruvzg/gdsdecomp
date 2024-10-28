7927cc89e9b027d5a05ac86bf634af40f5f51d63
nikitalita <69168929+nikitalita@users.noreply.github.com>
2024-10-28 06:04:04 -0500

    fix static checks

--- utility/common.h
+++ utility/common.h
@@ -4,7 +4,7 @@
 namespace gdre {
 bool check_header(const Vector<uint8_t> &p_buffer, const char *p_expected_header, int p_expected_len);
 Error ensure_dir(const String &dst_dir);
-}
+} // namespace gdre
 
 // Can only pass in string literals
 #define _GDRE_CHECK_HEADER(p_buffer, p_expected_header) gdre::check_header(p_buffer, p_expected_header, sizeof(p_expected_header) - 1)
