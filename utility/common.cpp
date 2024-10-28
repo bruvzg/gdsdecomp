08807cd06688487545a29e1365609f23435b04d2
nikitalita <69168929+nikitalita@users.noreply.github.com>
2024-10-28 17:40:04 -0500

    fix gdre::ensure_dir

--- utility/common.cpp
+++ utility/common.cpp
@@ -2,10 +2,13 @@
 #include "core/io/dir_access.h"
 
 Error gdre::ensure_dir(const String &dst_dir) {
-	Error err;
+	Error err = OK;
 	Ref<DirAccess> da = DirAccess::create(DirAccess::ACCESS_FILESYSTEM);
 	ERR_FAIL_COND_V(da.is_null(), ERR_FILE_CANT_OPEN);
-	err = da->make_dir_recursive(dst_dir);
+	// make_dir_recursive requires a mutex lock for every directory in the path, so it behooves us to check if the directory exists first
+	if (!da->dir_exists(dst_dir)) {
+		err = da->make_dir_recursive(dst_dir);
+	}
 	return err;
 }
 
