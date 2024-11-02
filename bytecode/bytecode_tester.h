4820f66919ebdf7cb9dfcd15675467a5412e6ca3
nikitalita <69168929+nikitalita@users.noreply.github.com>
2024-11-02 13:08:33 -0500

    remove not-applicable comment

--- bytecode/bytecode_tester.h
+++ bytecode/bytecode_tester.h
@@ -6,7 +6,6 @@
 
 class BytecodeTester {
 public:
-	// NOTE: This function is only implemented for testing 2.1 or 3.1 scripts. This returns 0 for everything else.
 	static uint64_t test_files(const Vector<String> &p_paths, int ver_major_hint = -1, int ver_minor_hint = -1);
 	static Vector<Ref<GDScriptDecomp>> filter_decomps(const Vector<Ref<GDScriptDecomp>> &decomps, int ver_major_hint, int ver_minor_hint);
 	static Vector<Ref<GDScriptDecomp>> get_possible_decomps(Vector<String> bytecode_files, bool include_dev = false);
