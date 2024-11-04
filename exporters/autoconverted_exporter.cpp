56597b81e1fc57329ea54cf6ab9e5e98b66e20c4
nikitalita <69168929+nikitalita@users.noreply.github.com>
2024-11-03 23:36:20 -0600

    refuse to convert 2.0 xml scenes

--- exporters/autoconverted_exporter.cpp
+++ exporters/autoconverted_exporter.cpp
@@ -1,5 +1,6 @@
 #include "autoconverted_exporter.h"
 #include "compat/resource_loader_compat.h"
+#include "core/error/error_list.h"
 #include "exporters/export_report.h"
 
 Error AutoConvertedExporter::export_file(const String &p_dest_path, const String &p_src_path) {
@@ -8,10 +9,15 @@ Error AutoConvertedExporter::export_file(const String &p_dest_path, const String
 
 Ref<ExportReport> AutoConvertedExporter::export_resource(const String &output_dir, Ref<ImportInfo> import_infos) {
 	// Check if the exporter can handle the given importer and resource type
-
+	String dst_ext = import_infos->get_export_dest().get_extension().to_lower();
 	String src_path = import_infos->get_path();
 	String dst_path = output_dir.path_join(import_infos->get_export_dest().replace("res://", ""));
 	Ref<ExportReport> report = memnew(ExportReport(import_infos));
+	if (import_infos->get_export_dest().get_extension().to_lower() == "xml") {
+		report->set_error(ERR_UNAVAILABLE);
+		report->set_unsupported_format_type("2.0 XML format");
+		return report;
+	}
 	Error err = export_file(dst_path, src_path);
 	report->set_error(err);
 	report->set_saved_path(dst_path);
