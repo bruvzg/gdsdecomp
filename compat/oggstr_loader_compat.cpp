1a53c04c62d6fb31fba8d746eeb8c737ad50f6a7
nikitalita <69168929+nikitalita@users.noreply.github.com>
2024-11-05 21:29:54 -0600

    fix bugs in converters

--- compat/oggstr_loader_compat.cpp
+++ compat/oggstr_loader_compat.cpp
@@ -1,4 +1,5 @@
 #include "oggstr_loader_compat.h"
+#include "core/error/error_macros.h"
 #include "modules/vorbis/resource_importer_ogg_vorbis.h"
 
 Ref<Resource> OggStreamConverterCompat::convert(const Ref<MissingResource> &res, ResourceInfo::LoadType p_type, int ver_major, Error *r_error) {
@@ -7,6 +8,7 @@ Ref<Resource> OggStreamConverterCompat::convert(const Ref<MissingResource> &res,
 	bool loop = res->get("loop");
 	double loop_offset = res->get("loop_offset");
 	Ref<AudioStreamOggVorbis> sample = ResourceImporterOggVorbis::load_from_buffer(data);
+	ERR_FAIL_COND_V_MSG(sample.is_null(), res, "Failed to load Ogg Vorbis stream from buffer.");
 	if (!name.is_empty()) {
 		sample->set_name(name);
 	}
