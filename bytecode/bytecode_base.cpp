80dd4bf9691edb77df9b2889ba154df4c292ebbf
nikitalita <69168929+nikitalita@users.noreply.github.com>
2024-11-17 04:54:04 -0600

    force use of spaces in gdscript 2.0

--- bytecode/bytecode_base.cpp
+++ bytecode/bytecode_base.cpp
@@ -666,31 +666,7 @@ Error GDScriptDecomp::decompile_buffer(Vector<uint8_t> p_buffer) {
 	int tab_size = 1;
 	bool use_spaces = false;
 	if (columns.size() > 0) {
-		Vector<int> distinct_col_values;
-		for (int i = 0; i < tokens.size(); i++) {
-			auto col_val = get_col_func(i);
-			if (col_val != 0 && distinct_col_values.find(col_val) == -1) {
-				distinct_col_values.push_back(col_val);
-			}
-		}
-		distinct_col_values.sort();
-		tab_size = INT_MAX; // minimum distance between columns = tab size
-		for (int i = 1; i < distinct_col_values.size(); i++) {
-			int new_tab_size = distinct_col_values[i] - distinct_col_values[i - 1];
-			if (i != 1 && new_tab_size != tab_size) {
-				tab_size = 1;
-				use_spaces = true;
-				break;
-			}
-			tab_size = MIN(tab_size, new_tab_size);
-		}
-		if (tab_size == INT_MAX) {
-			if (distinct_col_values.size() == 1) {
-				tab_size = MAX(distinct_col_values[0] - 1, 1);
-			} else {
-				tab_size = 1;
-			}
-		}
+		use_spaces = true;
 	}
 
 	auto handle_newline = [&](int i, GlobalToken curr_token, int curr_line, int curr_column) {
